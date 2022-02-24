4c862d264783855956fc880feddde3f4fca2b15f
Julianni Ramirez <JRamirez@Juliannis-Mini.attlocal.net>
2022-02-24 09:46:16 -0500

    skip tests were removed, docstrings were added to the test cases

--- broken_hashserve_api_tests.py
+++ broken_hashserve_api_tests.py
@@ -10,8 +10,11 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
         self.bhs_api = broken_hashserve_api.BrokenHashServeEndpoints()
         self.gp = generatePassword()
 
-    @unittest.skip("successful password post")
     def test_post_hash_password_succesful(self):
+        '''
+        See test case 1
+
+        '''
         # todo write a class or function that generates a password
         password = self.gp.generate_Password()
         password_payload = '{"password":"' + password + '"}'
@@ -21,8 +24,11 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
 
         self.assertIsInstance(test_response[1], str)
 
-    @unittest.skip("skipping invalid json")
     def test_pos_hash_password_invalid_json(self):
+        '''
+        See test case 2
+
+        '''
         password = self.gp.generate_Password()
         password_payload = '{"password":"' + password + '",}'
         test_response = self.bhs_api.post_hash_endpoint(password_payload)
@@ -33,8 +39,11 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
 
         self.assertEqual(test_response[1], "Malformed Input\n")
 
-    @unittest.skip("skipping empty string")
     def test_post_hash_password_emptystring(self):
+        '''
+        see test case 3
+
+        '''
         password = ""  # testing for empty string, this test case should fail since the server responds with 200
         password_payload = '{"password":"' + password + '"}'
         test_response = self.bhs_api.post_hash_endpoint(password_payload)
@@ -45,16 +54,22 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
 
         self.assertEqual(test_response[1], str)
 
-    @unittest.skip("skipping base64 password")
     def test_get_base64_password(self):
+        '''
+        see test case 4
+
+        '''
         test_response = self.bhs_api.get_base64_password(1)
 
         self.assertEqual(test_response[0], 200)
 
         self.assertIsInstance(test_response[1], str)
 
-    @unittest.skip("skipping base64 password hash_not_found")
     def test_get_base64_password_hash_not_found(self):
+        '''
+        see test case 5
+
+        '''
         test_response = self.bhs_api.get_base64_password(100000000)  # inserting an absurdly high number to
         # ensure string 'hash not found' is always returned
 
@@ -64,18 +79,23 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
 
         self.assertEqual(test_response[1], 'Hash not found\n')
 
-    @unittest.skip("skipping stats")
     def test_get_stats(self):
+        '''
+        see test case 6
+
+        '''
         test_response = self.bhs_api.get_stats()
 
         self.assertEqual(test_response[0], 200)
 
         self.assertIsInstance(test_response[1], dict)  # todo write a test that asserts for json not the dictionary type
 
-    @unittest.skip("skipping shutdown")
     def test_shutdown(self):
+        '''
+        see test case 7
+
+        '''
         test_response = self.bhs_api.shutdown()
-        # print(test_response, type(test_response))
 
         self.assertEqual(test_response, 200)
 
@@ -84,5 +104,6 @@ class TestBrokenHashServeAPITests(unittest.TestCase):
 
 if __name__ == 'main':
     unittest.main()
+
 
 
